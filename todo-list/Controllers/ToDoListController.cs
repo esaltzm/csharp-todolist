ca3f09d5ba087e78b7ffe6b058a69c26da3f7474
esaltzm <elisaltzman@gmail.com>
2023-02-02 15:16:03 -0500

    basic routes, get all items and post new item

---
+++ todo-list/Controllers/ToDoListController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using todo_list.Models;
+
+namespace todo_list.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ToDoListController : ControllerBase
+    {
+        private readonly ToDoList toDoList;
+
+        public ToDoListController()
+        {
+            toDoList = new ToDoList();
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(toDoList.GetTasks());
+        }
+
+        [HttpPost]
+        public IActionResult AddTask(Models.Task task)
+        {
+            toDoList.AddTask(task);
+            return Ok();
+        }
+    }
+}
---
+++ todo-list/ToDoListModels.cs
@@ -0,0 +1,27 @@
+﻿namespace todo_list.Models;
+
+public class Task
+{
+    public string? Description { get; set; }
+}
+
+public class ToDoList
+{
+    private List<Task> Tasks { get; set; }
+
+    public ToDoList()
+    {
+        Tasks = new List<Task>();
+    }
+
+    public void AddTask(Task task)
+    {
+        Tasks.Add(task);
+    }
+
+    public List<Task> GetTasks()
+    {
+        return Tasks;
+    }
+}
+
